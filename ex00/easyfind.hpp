d11ae80872e17e6834bfe65e771b98b5cb21e8bf
Carmine Watcher <cwatcher@student.21-school.r>
2021-11-24 01:31:55 +0300

    [done] ex00

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -1,1 +1,12 @@
+#pragma once
 #include <algorithm>
+#include <stdexcept>
+
+template< typename TContainer >
+typename TContainer::iterator	easyfind( TContainer& haystack, int needle )
+{
+	typename TContainer::iterator it = find( haystack.begin(), haystack.end(), needle );
+	if ( it == haystack.end() )
+		throw ( std::logic_error("Not found") );
+	return it;
+}
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -3,6 +3,21 @@
 #include <list>
 #include <iostream>
 
+template < typename TContainer >
+void	test()
+{
+	try
+	{	int	nums[] = { 3, 5, 7, 19, 21, 23, 42 };
+		TContainer	container (	nums,  nums + sizeof( nums ) / sizeof( nums[0] ) );
+		std::cout << *easyfind( container , 21 ) << std::endl;
+		std::cout << *easyfind( container , 22 ) << std::endl;
+	}
+	catch ( const std::exception& e )
+	{	std::cout << e.what() << std::endl;
+	}
+}
 int		main()
 {
+	test< std::vector< int > >();
+	test< std::list< int > >();
 }
