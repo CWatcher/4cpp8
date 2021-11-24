a7054c9547dbee563a538615f28b8772bf047973
Carmine Watcher <cwatcher@student.21-school.r>
2021-11-24 22:30:55 +0300

    + more tests

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,28 +1,29 @@
 #include "mutantstack.hpp"
 #include <iostream>
+#include <vector>
 
 int main()
 {
 	MutantStack<int> mstack;
-	mstack.push(5);
-	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
-	mstack.pop();
-	std::cout << mstack.size() << std::endl;
+	// mstack.push(5);
+	// mstack.push(17);
+	// std::cout << mstack.top() << std::endl;
+	// mstack.pop();
+	// std::cout << mstack.size() << std::endl;
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-	//[...]
-	mstack.push(0);
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
-	++it;
-	--it;
-	while (it != ite)
-	{
-	std::cout << *it << std::endl;
-	++it;
-	}
+	mstack.push(21);
+	mstack.push(42);
+	std::cout << mstack;
+	mstack.pop();
+	std::cout << "Pop(), Top  : " << mstack.top() << std::endl;
+	mstack.push( ~(-1U >> 1) );
+	std::cout << "Push(), Size : " << mstack.size() << std::endl;
 	std::stack<int> s(mstack);
+	MutantStack< int > mstack2( s );
+	MutantStack< int > mstack3;
+	mstack3 = mstack2;
+	std::cout << "After copy constructors(), operator=() :\n" << mstack3;
 	return 0;
 }
--- ex02/mutantstack.hpp
+++ ex02/mutantstack.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <stack>
+#include <ostream>
 
 template < typename T, typename TContainer = std::deque< T > >
 class MutantStack : public std::stack< T, TContainer > {
@@ -24,3 +25,16 @@ public:
 	{	return this->c.end();
 	}
 };
+
+template < typename T, typename TContainer >
+std::ostream&	operator<<( std::ostream& os, MutantStack< T, TContainer > mstack )
+{
+	typename MutantStack< T, TContainer >::iterator it = mstack.begin();
+	typename MutantStack< T, TContainer >::iterator end = mstack.end();
+	os << "{ ";
+	while (it != end) {
+		os << *it << " ";
+		++it;
+	}
+	return os << " }" << std::endl;
+}
