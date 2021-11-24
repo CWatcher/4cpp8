5078f55898e3ae8e2827111afb012c1bb6c1440f
Carmine Watcher <cwatcher@student.21-school.r>
2021-11-24 22:22:43 +0300

    + cannonical form; template += TContainer

--- ex02/mutantstack.hpp
+++ ex02/mutantstack.hpp
@@ -1,15 +1,26 @@
 #pragma once
 #include <stack>
 
-template < typename T >
-class MutantStack : public std::stack< T >
-{
-	public:
-		typedef typename std::stack< T >::container_type::iterator	iterator;
-		iterator begin()
-		{	return this->c.begin();
-		}
-		iterator end()
-		{	return this->c.end();
-		}
+template < typename T, typename TContainer = std::deque< T > >
+class MutantStack : public std::stack< T, TContainer > {
+public:
+	MutantStack()
+	{};
+	MutantStack( std::stack< T, TContainer > src )
+	:	std::stack< T, TContainer > ( src )
+	{}
+	~MutantStack()
+	{}
+	MutantStack& operator=( std::stack< T, TContainer > src )
+	{	return static_cast< std::stack< T, TContainer > >( *this ) = src;
+	}
+
+	typedef typename std::stack< T, TContainer >::container_type::iterator	iterator;
+
+	iterator begin()
+	{	return this->c.begin();
+	}
+	iterator end()
+	{	return this->c.end();
+	}
 };
